946173bbb20d2686e779a374c704d23b71a81b7f
Park Donghyeon <emma0415@naver.com>
2018-04-19 00:36:11 +0900

    Print progress. When Test finish, exit automatically.

--- main.cpp
+++ main.cpp
@@ -5,36 +5,36 @@
 #include <fstream>
 #include <sys/mman.h>
 #include <unistd.h>
-#include <signal.h>
+#include <stdlib.h>
 
-#define PERIOD 100000 //RT tasks period
+#define PERIOD 100000 //RT tasks period(ns)
 #define NUM 30 // number of RT tasks
-#define DNUM 1000000 // number of threads cycle
+#define DNUM 10000 // number of threads cycle
+
 RT_TASK measureTask;
 RT_TASK task[NUM];
-RTIME prev, now;
 std::ofstream file("./data.txt");
 RTIME *data = new RTIME[DNUM];
 
-void signal(int signo){
+void measureThread(void* arg){
+	int cnt = 0;
+	rt_task_set_periodic(NULL, TM_NOW, PERIOD);
+	while(cnt < DNUM){
+		rt_task_wait_period(NULL);
+		data[cnt] = rt_timer_read();
+		++cnt;
+	}
+
 	for(int i=0; i<NUM; ++i){
 		rt_task_delete(&task[i]);
 	}
 	for(int i=0; i<DNUM; ++i){
 		file << data[i] << std::endl;
 	}
-	delete[] data;
-}
 
-void measureThread(void* arg){
-	int cnt = 0;
-	rt_task_set_periodic(NULL, TM_NOW, PERIOD);//100us
-	while(cnt < DNUM){
-		rt_task_wait_period(NULL);
-		data[cnt] = rt_timer_read();
-		++cnt;
-	}
-	std::cout<< "Test End" << std::endl;
+	delete[] data;
+	std::cout<< "End of Test." << std::endl;
+	exit(1);
 }
 
 void dummyThread(void* arg){
@@ -47,17 +47,16 @@ void dummyThread(void* arg){
 }
 
 int main(int argc, char* argv[]){
-	signal(SIGUSR1, signal);
-
 	char RTName[] = "taskN";
 	mlockall(MCL_CURRENT | MCL_FUTURE);
 	
 	rt_task_create(&measureTask, "Measure Task", 0, 99, 0);
-
+	std::cout<< "create Measurement RT task" << std::endl;
+	
 	for(int i=0; i<NUM; ++i){
 		RTName[4] = '0'+i;
 		rt_task_create(&task[i], RTName, 0, 99, 0);
-		std::cout<< RTName << std::endl;
+		std::cout<< "create RT task: " << RTName << std::endl;
 
 	}
 	for(int i=0; i<NUM; ++i){
